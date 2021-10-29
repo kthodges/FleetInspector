86a5b8adce3cd95e2a9ddfad5b538468bd704ab4
kthodges <kthodges3@gmail.com>
2021-10-28 22:06:55 -0400

    Auth Settings

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {
+  Component, ChangeDetectorRef 
+} from '@angular/core';
+
+/* AWS AUTH */
+import {
+  onAuthUIStateChange,
+  CognitoUserInterface,
+  AuthState
+} from '@aws-amplify/ui-components';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FleetInspector';
+
+  /* AWS AUTH */
+  user: CognitoUserInterface | undefined;
+  authState!: AuthState;
+
+  /* AWS AUTH */
+
+  constructor(private ref: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData as CognitoUserInterface;
+      this.ref.detectChanges();
+    })
+  }
+
+  ngOnDestroy() {
+    return onAuthUIStateChange;
+  }
+
 }
